6d13742836010bceebc6aab52b2e116ba5ae3a3a
Spworld01 <mrsandeeppatil0011@gmail.com>
2026-04-25 15:59:00 +0530

    2nd

---
+++ 5th_practise.js
@@ -0,0 +1,50 @@
+// console.log("Sandeep Patil");
+// console.error("This is an error message");
+// console.warn("This is a warning message!");
+// let user={name:"Alice",age:25,city:"new York"};
+// console.table(user);
+// console.log(user);
+// console.count("Counter");
+// console.count("Counter");
+// console.count("counter");
+
+// // let age=24;
+// // age=23;
+// // console.log(age);
+
+
+// const age=24;
+// console.log(age);
+
+// Array
+// const participant=["Aayush","Ankur","Bhuwan","Abhishek"]
+// console.log(participant); 
+
+// const participant2=new Array("Aayush","Ankur","bhuw","Abhishek");
+// console.log(participant2);
+
+// const arr=["Something","abcd",1,"false","zzzzzzzzzzz","again","13337","true","1.4"];
+// arr[3.5]="ABCD Greeks";
+// console.log(arr[4]);
+// console.log(arr[7]);
+// console.log(arr[6-2]);
+// console.log(arr[6]-arr[2]);
+// console.log(arr[6]+arr[0]);
+// console.log(arr[7/2]);
+// console.log(arr[1000000000000000000000000000000]);
+
+// const arr=["A","B","C"];
+// console.log(arr[1]);
+
+// const arr1=["A","B","c"];
+// arr[1]="D";
+// console.log(arr1);
+
+// const arr2=['A',"B","c"];
+// let removed=arr2.pop();
+// console.log(removed);
+// console.log(arr2);
+
+// const user={ name:"Alice",age:25,Active:true}
+// console.log(user);
+
