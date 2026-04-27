cc5a95df9026112a8aba2cf1a0364a2d73b65078
Spworld01 <mrsandeeppatil0011@gmail.com>
2026-04-27 09:07:03 +0530

    4thday

---
+++ 9th.js
@@ -0,0 +1,35 @@
+// 1.Print all even numbers from 0 to 100 
+// for(let i=1;i<=100;i++) {
+//     if(i%2==0) {
+//         console.log(i);
+//     }
+// }
+// 2.Create a game where you start with any random game Number.Ask the user to keep guessing the game number until th user enters correct value 
+// let gamenum=25;
+// let usernum=prompt("Guess the number:");
+// while(usernum!=gamenum){
+//  usernum=prompt("You entered wrong number.guess again:");
+// }
+// console.log("Congraltions,you entered the right number");
+// 1.Prompt the user to enter their fullname.Generate a username for them based on the InputDeviceInfo.Start username with @AbortController,followed by their full name and ending with the fullname length.toExponential
+// ex:user name = "Sandeepprabhupatil",username should be "@Sandeepprabhupatil"
+//  let i=Math.random();
+//  let username = prompt("Enter your fullname");
+
+// console.log("@"+username+i);
+
+// For loop Pratise 
+// for(let i=1;i<=5;i++) {
+//     console.log(i);
+// }
+// let sum=0;
+// for(let i=2;i<=10;i+=2) {
+//     sum=sum+i;
+
+//     console.log(i);
+// }
+// console.log(sum);
+
+for(let i=5;i>=1;i--) {
+    console.log(i);
+}
